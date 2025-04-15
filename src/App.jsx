892d8cbf083ce85645c2a9479d7278fae54565fd
Lalitmehra9720 <139990443+Lalitmehra9720@users.noreply.github.com>
2025-04-15 18:40:36 +0530

    Footer and main page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "au-css/dest/index.css"; // Main styles
 import "au-css/js/arbitary-values.js"; // Optional for custom utilities
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <div>
       <Navbar/>
         <Home/>
+        <Footer/>
         
     </div>
    
---
+++ src/component/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex justify-center bg-[#042a58] w-[98.5vw] h-auto p-[10px] gap-[35px] capitalize text-[20px] text-white overflow-hidden'>
+          <p className='cursor-pointer'>support</p>
+          <p className='cursor-pointer'>privacy</p>
+          <p className='cursor-pointer'>terms</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
